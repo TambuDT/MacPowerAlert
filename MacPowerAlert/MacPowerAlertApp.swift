81e17fff2286c5dec2d64b6948120926453331e1
Davide Tamburrelli <58508206+TambuDT@users.noreply.github.com>
2024-01-02 21:30:04 +0100

    Initial Commit

---
+++ MacPowerAlert/MacPowerAlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacPowerAlertApp.swift
+//  MacPowerAlert
+//
+//  Created by Davide Tamburrelli on 02/01/24.
+//
+
+import SwiftUI
+
+@main
+struct MacPowerAlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
